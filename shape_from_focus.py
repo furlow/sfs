cc45f60cb9f1f82b72e8e0010e9e50bdddb47f64
Joshua Furlow <jf00109@surrey.ac.uk>
2013-11-12 22:44:21 +0000

    Added image alignment and many other improvment features

--- shape_from_focus.py
+++ shape_from_focus.py
@@ -4,38 +4,54 @@
 from shape_from_focus_lib import *
 import cv2
 from matplotlib import pyplot as plt
+from matplotlib import cm
 import os
 import time
 import sort
+import subprocess
 
 #main section
-
-img_dir = 'images/stack/'
-
-#list of image files and depth
+print('Creating temporary directories...')
+img_dir = 'images/book/'
+tmp_copy = 'tmp_copy/'
+tmp_cropped = 'tmp_cropped/'
+output = 'output/'
 image_files = os.listdir(img_dir)
+print(image_files)
 sort.sort_nicely(image_files)
-stack_depth = len(image_files)
+print(image_files)
+
+#Check if the folders have been created and images cropped
+if( not os.path.exists(img_dir + tmp_copy) and not os.path.exists(img_dir + tmp_cropped) and  not os.path.exists(img_dir + output) ):
+	os.mkdir(img_dir + tmp_copy)
+	os.mkdir(img_dir + tmp_cropped)
+	os.mkdir(img_dir + output)
+	
+	align_image_stack_cmd = ["align_image_stack", "-v", "-m", "-C", "-a", img_dir + tmp_cropped,]
+	
+	for file in image_files:
+		shutil.copyfile(img_dir + file, img_dir + tmp_copy + file)
+		align_image_stack_cmd.append(img_dir + tmp_copy + file)
+	
+	#Align image files to remove focus magnification
+	print('Aligning images...')
+	subprocess.call(align_image_stack_cmd)
 
 #Create a new image stack
-stack = image_stack(stack_depth, 302, 302)
+print('Creating image stack...')
+stack = image_stack(img_dir + tmp_cropped) #setup an image stack
 
-#Add all the images in the folder to the stack
-for file in image_files:
-	print(file)
-	stack.add(img_dir + file)
-
+#Display images
 #for position in range(0, stack.depth):
-#	plt.imshow(stack.image[position])
+#	plt.imshow(stack.image[position], cmap = cm.Greys_r)
 #	plt.show()
 
-#Generate Depth map
+#Generate Depth map, need to add time calculation 
 start = time.time()
-
+print('Generating Depth map...')
 create_depth_map(stack)
-
 finish = time.time()
 
 print 'Execution time =',finish-start
 
-cv2.imwrite( img_dir + 'out.jpg', stack.depth_map)
+cv2.imwrite( img_dir + output + 'out.jpg', stack.depth_map)
--- shape_from_focus_lib.py
+++ shape_from_focus_lib.py
@@ -1,19 +1,35 @@
 #Import various libaries
 import cv2
 import numpy as np
+import sort
+import shutil
+import os
 
 #This is the image stack class
 class image_stack:
 	current_depth = 0
 
 	#Initalization function
-	def __init__(self, depth, height, width):	
-		self.height = height
-		self.width = width
-		self.depth = depth
-		self.stretch_factor = 256 / depth
-		self.depth_map = np.zeros( (height, width), dtype=np.float64)
-		self.image = np.zeros((depth, height, width), dtype=np.float64)
+	def __init__(self, img_dir):
+		#Move this into a function which also checks if all the files have the same dimension sizes
+		image_files = os.listdir(img_dir)
+		sort.sort_nicely(image_files)
+		
+		#Get image size of first file and assume as dimensions of all files
+		img = cv2.imread(img_dir + image_files.pop(0), 0)
+		self.height, self.width = img.shape
+
+		self.depth = len(image_files)
+		self.stretch_factor = 255 / self.depth - 1
+		
+		#Create depth map and image stack arrays
+		self.depth_map = np.zeros((self.height, self.width), dtype=np.float64)
+		self.image = np.zeros((self.depth, self.height, self.width), dtype=np.float64)
+		
+		#Add the files
+		for file in image_files:
+				print('Adding ' + file)
+				self.add(img_dir + file)
 
 	#This function is used to add an image to the stack
 	def add(self, img_file):
@@ -23,12 +39,12 @@ class image_stack:
 		img = img.astype(dtype=np.float64)
 
 		#find the focus using laplacian operator, this needs to be improved
-		img = cv2.Laplacian(img, -1)
+		img = cv2.Laplacian(img, -1, ksize = 1)
 		
 		#copy image to array
 		self.image[self.current_depth] = img
 		self.current_depth += 1
-
+		
 #Function for determining the focus maximum of a pixel
 def detect_focus_peak(stack, y, x):
 #possible create a focus structure 
@@ -38,8 +54,8 @@ def detect_focus_peak(stack, y, x):
 	grad_prev = 0
 	grad = 0
 	
-	for depth in range(0, stack.depth):
-		grad = stack.image[depth, y, x] - stack.image[depth - 1, y, x]
+	for depth in xrange(0, stack.depth - 1):
+		grad = stack.image[depth + 1, y, x] - stack.image[depth, y, x]
 		#Check for peak focus
 		if( grad == 0 or (grad < 0 and grad_prev > 0)):
 			#Look for the peak with heighest focus
@@ -50,7 +66,7 @@ def detect_focus_peak(stack, y, x):
 
 #Function for generating a depth map from a a focus image stack class
 def create_depth_map(stack):
-	for x in range(0, stack.width):
-		for y in range(0, stack.height):
+	for x in xrange(0, stack.width):
+		for y in xrange(0, stack.height):
 			#print(str(x) + "." + str(y))
 			stack.depth_map[y,x] = detect_focus_peak(stack, y, x)
