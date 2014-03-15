e54eb8a9acaea031600c43723d7f877b227f8940
Joshua Furlow <englandrules1992@gmail.com>
2014-03-15 18:04:36 +0000

    Code rewrite finished switching from OpenCV to jbutil
    - Now using the jbutil image container inplace on the OpenCV Mat
    - Implemented a box filter funciton inplace of the OpenCV function
    - Removed unecessary functions and data, more clean up would be good

--- depth_map_methods.cpp
+++ depth_map_methods.cpp
@@ -2,54 +2,43 @@
 #include <time.h>
 #include <cmath>
 #include <exception>
-#include <vector>
+using namespace std;
 
 //image stack constructor
-image_stack::image_stack(int height, int width, int size, int threshold, char* output_img_dir):
-height(height), width(width), size(size), threshold(threshold) , output_img_dir(output_img_dir), SML(height, width, 9)
-{
-    depth_map = Mat(height, width, CV_32F);
-    focused = Mat(height, width, CV_8UC3);
-    refocused = Mat(height, width, CV_8UC3);
-}
+image_stack::image_stack(int height, 
+						int width,
+						int size,
+						int threshold,
+						char* output_img_dir):
+						height(height),
+						width(width),
+						size(size),
+						threshold(threshold),
+						output_img_dir(output_img_dir),
+						SML(height, width, 9),
+						depth_map(height, width){}
     
 //This function is used to add an image to the stack
 void image_stack::add(char* image_path)
 {
-    
-    Mat colour_image, img_32f, gray_image;
+    jbutil::image<float> image_in;
         
     cout << image_path << endl;
-    
-    clock_t init, final;
-    
-    init=clock();
-
-    colour_image = imread(image_path);
-    
-    final=clock()-init;
-    cout << "Read image " << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-    raw_stack.push_back(colour_image.clone());
-        
-    init=clock();
-    
-    cvtColor(colour_image, gray_image, CV_BGR2GRAY);
-    gray_image.convertTo(img_32f, CV_32F);
-    
-    final=clock()-init;
-    cout << "Convert image to float " << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-    init=clock();
-    
-    focus_map_stack.push_back(SML(img_32f).clone());
-    
-    final=clock()-init;
-    cout << "Run SML and push into the stack " << (double)final / ((double)CLOCKS_PER_SEC) 
-    << endl;
+	
+    // load image
+    ifstream file_in(image_path);
+    image_in.load(file_in);
+	
+	// TODO probably going need to clone image_in as it will most certainly be 
+	// referenced in the push_back function, maybe use dynamic memory mangement
+	// when creating the image above
+    focus_map_stack.push_back(SML(image_in));
 }
 
-//Function for determining the focus maximum of a pixel
+/* Function for determining the focus maximum of a pixel
+ * and thus estimating the depth in the scene at that
+ * pixel
+ */
 inline float image_stack::coarse_depth_esstimation(int y, int x)
 {
     int max_focus_depth = 0;
@@ -57,13 +46,10 @@ inline float image_stack::coarse_depth_esstimation(int y, int x)
     
     for(int z = 0; z < focus_map_stack.size(); z++)
     {
-
-        float* focus = focus_map_stack[z].ptr<float>(y,x);
-
-        if( *focus > max_focus)
+        if( focus_map_stack[z](0,y,x) > max_focus)
         {
             max_focus_depth = z;
-            max_focus = *focus;
+            max_focus = focus_map_stack[z](0,y,x);
         }
     }
     
@@ -77,299 +63,83 @@ inline float image_stack::coarse_depth_esstimation(int y, int x)
 		
 }
 
-//Function for determining the focus maximum of a pixel
-inline float image_stack::gaussian_depth_esstimation(int y, int x)
-{
-    int dm = 0;
-    float max_focus = 0;
-    float max_focus_minus = 0;
-    float max_focus_plus = 0;
-    
-    for(int z = 2; z < focus_map_stack.size(); z++)
-    {
-        //find peak
-        if( *(focus_map_stack[z - 1].ptr<float>(y,x)) > max_focus //&&
-            //*(stack[z - 1].ptr<float>(y,x)) > *(stack[z].ptr<float>(y,x)) //&& 
-             //stack[z - 1].at<float>(y,x) > stack[z - 2].at<float>(y,x)
-          )
-        {
-            max_focus = *(focus_map_stack[z - 1].ptr<float>(y,x));
-            dm = z - 1;
-        }
-    }
-    
-    int dmm = dm - 1;
-    int dmp = dm + 1;
-
-    max_focus_minus = *(focus_map_stack[dmm].ptr<float>(y,x));
-    max_focus_plus = *(focus_map_stack[dmp].ptr<float>(y,x));
-    
-    float depth = depth_mean(max_focus, max_focus_plus, max_focus_minus, dm, dmp, dmm);
-    
-    if(max_focus > threshold && depth < size && depth >= 0){
-    	//cout << depth << endl;
-    	return depth;
-	}
-	else
-	{
-		return size - 1;
-	}
-}
-
-
-inline float image_stack::depth_mean
-(float Fm, float Fmp, float Fmm, int dm, int dmp, int dmm)
-{
-    float log_Fm = log(Fm);
-    float log_Fmm = log(Fmm);
-    float log_Fmp = log(Fmp);
-    float log_fm_minus_fmp = (log_Fm - log_Fmp);
-    float log_fm_minus_fmm = (log_Fm - log_Fmm);
-    float dm_sq = pow(dm, 2);
-
-    float d_mean = (
-                  log_fm_minus_fmp*(dm_sq - pow(dmm, 2))
-                  -
-                  log_fm_minus_fmm*(dm_sq - pow(dmp, 2))
-                 )
-                 /
-                 ( 2 * (log_fm_minus_fmm + log_fm_minus_fmp) );
-    
-    return d_mean;
-}
-
 //Function for generating a depth map
 void image_stack::create_depth_map()
 {
-    /*
-    for(int z = 0; z < stack.size(); z++)
-    {
-        cout << "image " << z << endl;
-        convertScaleAbs(stack[z],dst, 0.1);
-        resize(dst, dst, Size(), 0.1, 0.1);
-        imshow("Depth Map", dst);
-        waitKey(0);
-    }
-    */
-
     for(int y = 0; y < height; y++)
-    {
-        
-        float* y_ptr = depth_map.ptr<float>(y);
-        
+    {        
         for(int x = 0; x < width; x++)
         {
-            y_ptr[x] = gaussian_depth_esstimation(y, x);
+            depth_map( 0, y, x) = coarse_depth_esstimation(y, x);
         }
     }
     
     //Clear out the focus map stack as the data is no longer needed
     focus_map_stack.clear();
     
-    //Display the output image
-    
-    dst = depth_map * (255.0 / (size - 1));
-    
-    dst.convertTo(dst, CV_8U);
-
     //Save the image to file
-    cout << "Saving depth map to file" << endl;
-    imwrite( output_img_dir + "depth_map.jpg", dst);
-    
-    resize(dst, dst, Size(), 0.3, 0.3);
-    namedWindow( "Depth Map", WINDOW_AUTOSIZE );
-    imshow("Depth Map", dst);
-    
-
+    ofstream file_out( (output_img_dir + "depth_map.jpg").c_str());
+    depth_map.save(file_out);
 }
 
-void image_stack::fuse_focus(){
-    
-    Vector<Vec3b*> raw_stack_y_ptr;
-    
-    cout << "Running fuse focus" << endl;
-
-    for(int y = 0; y < height; y++)
-    {
-        
-        Vec3b* focused_y_ptr = focused.ptr<Vec3b>(y);
-        float* depth_map_y_ptr = depth_map.ptr<float>(y);
-        
-        for(int i = 0; i < size; i++){
-        	raw_stack_y_ptr.push_back( raw_stack[i].ptr<Vec3b>(y) );
-        }
-        
-        //Add pointer array of each image row in the raw_stack
-        
-        for(int x = 0; x < width; x++)
-        {
-        	//cout << int(depth_map_y_ptr[x]) << endl;
-            focused_y_ptr[x] = raw_stack_y_ptr[ int(depth_map_y_ptr[x]) ][x];
-
-        }
-        
-        raw_stack_y_ptr.clear();
-    }
-    
-    //Clear out the raw_stack as the data is no longer needed
-    raw_stack.clear();
-    
-    //Display the fused focus image
-    resize(focused, dst, Size(), 0.2, 0.2);
-    namedWindow( "Fused Focus", WINDOW_AUTOSIZE );
-    imshow("Fused Focus", dst);
+// Sum Modified Laplacian focus measure
+jbutil::image<float> sum_modified_laplacian::operator()(jbutil::image<float>& img){
      
-    //Save the fused focus image to file   
-    cout << "Saving fused image to file" << endl;
-    imwrite( output_img_dir + "fused_focus.jpg", focused );
-}
-
-void image_stack::generate_blurred_images(){
-
-	clock_t init, final;
-    init=clock();
-    
-    //for loop which blurs the infocus image from 0 now blur to the max blur required
-    for(int z = 0; z < size; z++){
-    	GaussianBlur(focused, dst, Size(z * 6 + 1, z * 6 + 1), 0, 0);
-    	//boxFilter(focused, dst, -1, Size(z * 6 + 1, z * 6 + 1));
-    	blurred.push_back(dst.clone());
-    }
-    
-	final = clock() - init;
-    cout << "blur images" << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-}
-
-//function used to artificially refocus an image
-void image_stack::refocus(int depth_of_field, int depth_focus_point){
-
-	clock_t init, final;
-    init=clock();
-
-	if(blurred.size() != size) generate_blurred_images();
+    cout << "Run SML algorithm " << endl;
 	
-    Vec3b* blurred_stack_y_ptr[size];
-         
-    //for loop which seVelects the correct pixels based on the depth map value
-    for(int y = 0; y < height; y++)
-    {
-        
-        Vec3b* refocused_y_ptr = refocused.ptr<Vec3b>(y);
-        float* depth_map_y_ptr = depth_map.ptr<float>(y);
-        
-        for(int i = 0; i < size; i++){
-        	blurred_stack_y_ptr[i] = blurred[i].ptr<Vec3b>(y);
-        }
-        
-        //Add pointer array of each image row in the raw_stack
-        
-        for(int x = 0; x < width; x++)
+    for(int y = step; y < height - step; y++)
+    {   
+        for(int x = step; x < width - step; x++)
         {
-            refocused_y_ptr[x] = blurred_stack_y_ptr[ abs(int(depth_map_y_ptr[x]) - depth_focus_point) ][x];
-
+            ML(0, y, x)
+            =
+            abs( 2 * img(0, y, x) - img(0, y, x - step) - img(0, y, x + step) )
+            +
+            abs( 2 * img(0, y, x) - img(0, y - step, x) - img(0, y + step, x) );
         }
     }
+	
+	//TODO implement a box filter function
+    SML = boxFilter(ML, step);
+    cout << "Run Box filter " << endl;
     
-    final = clock() - init;
-    cout << "Refocus image" << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-    init=clock();
-    
-    //Display the refocused image
-    resize(refocused, dst, Size(), 0.3, 0.3);
-    imshow("Fused Focus", dst);
-    
-    final = clock() - init;
-    cout << "Displaying image" << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-        
-    //Save the refocused image to file
-    //cout << "Saving fused image to file" << endl;
-    //imwrite( output_img_dir + "refocused.jpg", refocused );
-    
-    
+    return SML;
 }
 
-//This function is most likely obsolete
-inline void image_stack::boxfilter_single_pixel(int y, int x, int ksize){
-
-	int ksize_half = (ksize / 2);
-	int ksize_sq = ksize * ksize;
-	//cout << "ksize_half " << ksize_half << endl;
-	int b_sum = 0, g_sum = 0, r_sum = 0;
-	int lower_lim_col, lower_lim_row;
+/* This is a box filter function that will box filter
+ * an image using the seperable filter method of adding
+ * the image first in the y direction and then in the x
+ * direction.
+ * The filtering process can be accelerated by keeping a 
+ * total of the average in each direction and just subtract 
+ * the last value in the filter and add the new value.
+ */
+jbutil::image<float> boxFilter(const jbutil::image<float>& img, int step){
 	
-	if(y - ksize_half < 0){ 
-		lower_lim_row = 0;	
-	}
-	else
-	{
-		lower_lim_row = ksize_half;
-	}
+	jbutil::image<float> y_filtered(img.get_cols(), img.get_rows()), 
+				 result(img.get_cols(), img.get_rows());
 	
-	if(x - ksize_half < 0){
-		lower_lim_col = 0;
-	}
-	else
-	{
-		lower_lim_col =  ksize_half;
-	}
-		
-	for(int row = y - lower_lim_row; row <= (y + ksize_half) && row < focused.rows; row++)
-	{
-		Vec3b* row_ptr = focused.ptr<Vec3b>(row);
-		for(int col = x - lower_lim_col; col <= (x + ksize_half) && col < focused.cols; col++)
-		{
-			b_sum += row_ptr[col][0];
-			g_sum += row_ptr[col][1];
-			r_sum += row_ptr[col][2];
+	//Loop over every pixel and sum in y direction
+	for(int y = step; y < img.get_rows() - step; y++){
+		for(int x = step; x < img.get_cols() -  step; x++){
+			//Apply the vertical box filter here
+			//y_filtered(0, y, x) = 0; //zero the image before adding
+			for(int y_offset = - step; y_offset <= step*2; y_offset++){
+				y_filtered(0, y, x) += img(0,y + y_offset,x);
+			}
 		}
 	}
 	
-	Vec3b* pixel = refocused.ptr<Vec3b>(y);
-
-	pixel[x][0] = (b_sum / ksize_sq);
-	pixel[x][1] = (g_sum / ksize_sq);
-	pixel[x][2] = (r_sum / ksize_sq);
-}
-
-/* Sum Modified Laplacian focus measure
- */
-Mat sum_modified_laplacian::operator()(Mat& image){
-    
-    clock_t init, final;
-    
-    init=clock();
-        
-    for(int y = step; y < height - step; y++)
-    {
-        float* ML_ptr = ML.ptr<float>(y);
-        float* img_ptr = image.ptr<float>(y);
-        float* img_ptr_step_minus = image.ptr<float>(y - step);
-        float* img_ptr_step_plus = image.ptr<float>(y + step);
-        
-        for(int x = step; x < width - step; x++)
-        {
-            ML_ptr[x]
-            =
-            abs( 2 * img_ptr[x] - img_ptr[x - step] - img_ptr[x + step])
-            +
-            abs( 2 * img_ptr[x] - img_ptr_step_minus[x] - img_ptr_step_plus[x]);
-            
-            //cout << ML_ptr[x] << endl;
-        }
-    }
-
-    
-    final=clock()-init;
-    cout << "Run SML algorithm " << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-    init=clock();
-    
-    boxFilter(ML, SML, -1, Size(2*step+1,2*step+1), Point(-1,-1), false);
-
-    final=clock()-init;
-    cout << "Run Box filter " << (double)final / ((double)CLOCKS_PER_SEC) << endl;
-    
-    return SML;
+	//Loop over every pixel and sum in x direction
+	for(int y = step; y < img.get_rows() - step; y++){
+		for(int x = step; x < img.get_cols() -  step; x++){
+			//Apply the horizontal box filter here
+			//result(0, y, x) = 0; //zero the image before adding
+			for(int x_offset = - step; x_offset <= step*2; x_offset++){
+				result(0, y, x) = y_filtered(0,y,x + x_offset);
+			}
+		}
+	}
+	
+	return result;
 }
--- depth_map_methods.h
+++ depth_map_methods.h
@@ -1,21 +1,21 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <list>
 #include <string>
+#include <vector>
+#include "jbutil.h"
 
 using namespace std;
-using namespace cv;
-
-/* How to define a focus measure class ??
- The class needs to be overidable and customizable for implementing
- the different meausres
- Use a derived class which inherits from the base class focus measure
- focus measure will have a few virtual methods which can then be overidden
+     
+/* The focus_measure class is pure virutal and it allows for any valid focus
+ * measures to be implemented and used without out changing the underlying code
+ * base
+ * To define a focus measure create a derived class which inherits from the base
+ * class focus_measure with public inheritance and overide the operator()
+ * function with the focus measure you intend to implement
  */
 class focus_measure{
 public:
-    virtual Mat operator()(Mat& image) = 0;
+    virtual jbutil::image<float> operator()(jbutil::image<float>& img) = 0;
 };
 
 /* Sum Modified Laplacian focus measure
@@ -23,22 +23,28 @@ public:
 class sum_modified_laplacian: public focus_measure{
 public:
     sum_modified_laplacian
-    (int height, int width, int step):step(step), height(height), width(width){
-    
-        ML = Mat(height, width, CV_32F);
-        SML = Mat(height, width, CV_32F);
-    
+    (int height, int width, int step):step(step), height(height), width(width), 
+	ML(height, width), SML(height, width){
     }
-    Mat operator()(Mat& image);
+    jbutil::image<float> operator()(jbutil::image<float>& img);
 private:
     int step;
     int height;
     int width;
-    Mat ML; //need to remove these when un needed
-    Mat SML;
+    jbutil::image<float> ML; //need to remove these when un needed
+    jbutil::image<float> SML;
 };
 
-//This is the image stack class
+
+/* boxFilter function for use in the Sum Modified Laplacian
+ * focus measure filter
+ */
+jbutil::image<float> boxFilter(const jbutil::image<float>& img, int step);
+
+
+/* This is the image stack class it is used to contain an image stack
+ * for prcoessing into a depth map
+ */
 class image_stack{
 public:
     //Constructor
@@ -53,40 +59,13 @@ public:
     //Function for determining the focus maximum of a pixel
     //using coarse depth esstimation method
     inline float coarse_depth_esstimation(int y, int x);
-    
-    //Function for determing the focus maximum of a pixel
-    //using guassian interpolation
-    inline float gaussian_depth_esstimation(int y, int x);
-    
-    //Mean depth interpolation calculations
-    inline float depth_mean
-    (float Fm, float Fmp, float Fmm, int dm, int dmp, int dmm);
-    
-    //Function for generating an all in focus image
-    void fuse_focus();
-    
-    //Function for artificially refocusing an image
-    void refocus(int depth_of_feild, int depth_focus_point);
-    
-    //Function for generating blurred images for use in the refocus algorithm
-    void generate_blurred_images();
-
-    
-    //Function used to apply boxfilter blur to a single pixel
-    inline void boxfilter_single_pixel(int y, int x, int ksize);
-    
 private:
     int height;
     int width;
     int size;
     float threshold;
     string output_img_dir;
-    vector<Mat> raw_stack;
-    vector<Mat> focus_map_stack;
-    vector<Mat> blurred;
+    vector< jbutil::image<float> > focus_map_stack;
+    jbutil::image<float> depth_map;
     sum_modified_laplacian SML;
-    Mat focused;
-    Mat refocused;
-    Mat depth_map;
-    Mat dst; //temporary storage of an array
 };
