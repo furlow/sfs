65ee28447f7f5a71dbe0ae064bf756edc7fcbfad
Joshua Furlow <englandrules1992@gmail.com>
2014-04-09 16:26:54 +0100

    added image coverter python program

--- image_convert.py
+++ image_convert.py
@@ -9,5 +9,7 @@ images = [image for image in os.listdir(img_dir) if image.endswith('.JPG')]
 for image in images:
     with Image(filename=img_dir + image) as img:
         name, ext = image.split('.')
+        print('Resizing ' + name)
+        img.resize(int(img.width * 0.6), int(img.height * 0.6) )
         print('Converting ' + name)
         img.save(filename=img_dir + name + '.pgm')
