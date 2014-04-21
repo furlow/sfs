4e40843923ff06bcac237ebbc17f7a691436d9a1
Joshua Furlow <englandrules1992@gmail.com>
2014-04-21 23:54:55 +0100

    Completed numpy to qpixmap

--- src/gui.py
+++ src/gui.py
@@ -3,24 +3,31 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 from shape_from_focus import process_stack
+import cv2
 
 __appname__ = "Shape From Focus Refocus"
 
 class refocused_image(QLabel):
-    def __init__(self, parent = None):
+    def __init__(self, stack, parent = None):
         super(refocused_image, self).__init__(parent)
 
+        self.stack = stack
+
         #Set up the image pixmap
-        self.refocused_pixmap = QPixmap(400, 300)
+        self.refocused_pixmap = numpy2qpixmap( self.stack.focused_image )
         self.setPixmap(self.refocused_pixmap)
 
     def mousePressEvent(self, QMouseEvent):
-        print QMouseEvent.pos()
+        self.pos = QMouseEvent.pos()
+        type(self.pos)
+        print self.pos
+
 
 class Program(QDialog):
-    def __init__(self, parent = None):
+    def __init__(self, stack, parent = None):
         super(Program, self).__init__(parent)
 
+        self.stack = stack
         self.setWindowTitle(__appname__)
 
         #Set up the refocusing sliders
@@ -41,17 +48,28 @@ class Program(QDialog):
         options_layout.addWidget(self.dof)
 
         layout = QHBoxLayout()
-        layout.addWidget(refocused_image())
+        layout.addWidget(refocused_image(stack))
         layout.addItem(options_layout)
 
         self.setLayout(layout)
+
+def numpy2qpixmap(cvBGRImg):
+
+    #convert from BGR to RGB
+    cvRGBImg = cv2.cvtColor(cvBGRImg, cv2.CV_BGR2RGB)
+
+    #convert numpy to pixmap image
+    qimg = QtGui.QImage(cvRGBImg.data,cvRGBImg.shape[1], cvRGBImg.shape[0], QtGui.QImage.Format_RGB888)
+    Qpixmap_Img = QtGui.QPixmap.fromImage(qimg)
+
+    return Qpixmap_Img
 
 def main():
     sys.argv[0] = "Shape From Focus"
     img_dir = sys.argv[1]
     stack  = process_stack(img_dir)
     app = QApplication(sys.argv)
-    form = Program()
+    form = Program(stack)
     form.show()
     app.exec_()
 
