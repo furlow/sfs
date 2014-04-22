c89da8560c309da3bcb1bf15b1a35c8ffee10a15
Joshua Furlow <englandrules1992@gmail.com>
2014-04-22 15:15:14 +0100

    fixed unaccessible members of Pyimage cython class

--- src/depth_map_methods_py.pyx
+++ src/depth_map_methods_py.pyx
@@ -25,6 +25,8 @@ cdef class Pyimage_stack:
 	cdef image_stack *thisptr
 	cdef int height
 	cdef int width
+	cdef public np.ndarray focused_image
+	cdef public np.ndarray refocused_image
 	def __cinit__(self, int height, int width, int size, int threshold, char* output_img_dir):
 		self.thisptr = new image_stack(height, width, size, threshold, output_img_dir)
 		self.height = height
@@ -35,13 +37,15 @@ cdef class Pyimage_stack:
 		self.thisptr.add(image_path)
 	def create_depth_map(self):
 		self.thisptr.create_depth_map()
-	def fuse_focus(self): #, np.ndarray[char, mode="c"] fuse_focused_image):
-		cdef np.ndarray[char, ndim=3, mode="c"] fuse_focused_image
-		fuse_focused_image = np.zeros((self.height, self.width, 3), dtype = DTYPE, order = 'c')
-		self.thisptr.fuse_focus( &fuse_focused_image.data[0] )
-		return fuse_focused_image
-	def refocus(self, int depth_of_field, int depth_focus_point): #, np.ndarray[char, mode="c"] refocused_image):
-		cdef np.ndarray[char, ndim=3, mode="c"] refocused_image
-		refocused_image = np.zeros((self.height, self.width, 3), dtype = DTYPE, order = 'c')
-		self.thisptr.refocus(depth_of_field, depth_focus_point, &refocused_image.data[0] )
-		return refocused_image
+	def fuse_focus(self):
+		cdef np.ndarray[char, ndim=3, mode="c"] focused_image_local
+		focused_image_local = np.zeros((self.height, self.width, 3), dtype = DTYPE, order = 'c')
+		self.thisptr.fuse_focus( &focused_image_local.data[0] )
+		self.focused_image = focused_image_local
+		return focused_image_local
+	def refocus(self, int depth_of_field, int depth_focus_point):
+		cdef np.ndarray[char, ndim=3, mode="c"] refocused_image_local
+		refocused_image_local = np.zeros((self.height, self.width, 3), dtype = DTYPE, order = 'c')
+		self.thisptr.refocus(depth_of_field, depth_focus_point, &refocused_image_local.data[0] )
+		self.refocused_image = refocused_image_local
+		return refocused_image_local
